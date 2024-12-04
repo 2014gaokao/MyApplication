5caebc8babf1fe75653e8679b9713c25a067de4c
2014gaokao <2225052023@qq.com>
2024-12-04 23:02:48 +0800

    commit 18

--- app/src/main/java/com/example/myapplication/CameraFragment.kt
+++ app/src/main/java/com/example/myapplication/CameraFragment.kt
@@ -1,16 +1,29 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.media.ImageReader
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.example.myapplication.databinding.FragmentCameraBinding
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 class CameraFragment : Fragment() {
 
@@ -18,6 +31,33 @@ class CameraFragment : Fragment() {
     private var _fragmentCameraBinding: FragmentCameraBinding? = null
 
     private val fragmentCameraBinding get() = _fragmentCameraBinding!!
+
+    private val navController : NavController by lazy {
+        Navigation.findNavController(requireActivity(), R.id.fragment_container)
+    }
+
+    private val cameraManager: CameraManager by lazy {
+        val context = requireContext().applicationContext
+        context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    private val characteristics: CameraCharacteristics by lazy {
+        cameraManager.getCameraCharacteristics(arguments?.getString("camera_id").toString())
+    }
+
+    private lateinit var imageReader: ImageReader
+
+    private val cameraThread = HandlerThread("CameraThread").apply { start() }
+
+    private val cameraHandler = Handler(cameraThread.looper)
+
+    private val imageReaderThread = HandlerThread("imageReaderThread").apply { start() }
+
+    private val imageReaderHandler = Handler(imageReaderThread.looper)
+
+    private lateinit var camera: CameraDevice
+
+    private lateinit var session: CameraCaptureSession
 
     private lateinit var viewModel: UserViewModel
     //private val viewModel: UserViewModel by viewModels()
@@ -33,6 +73,22 @@ class CameraFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+//        fragmentCameraBinding.viewFinder.holder.addCallback(object : SurfaceHolder.Callback {
+//            override fun surfaceCreated(p0: SurfaceHolder) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun surfaceDestroyed(p0: SurfaceHolder) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        })
+
         viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
         viewModel.user.observe(viewLifecycleOwner, Observer { newData -> Log.d("zcc", newData.name) })
 
@@ -47,6 +103,9 @@ class CameraFragment : Fragment() {
             viewModel.updateUser("蔡徐坤")
         }
 
+        var camera_id = getArguments()?.getString("camera_id")
+        Log.d("zcc", camera_id.toString())
+
 //        runBlocking {
 //            val job = launch {
 //                delay(3000)
@@ -54,6 +113,27 @@ class CameraFragment : Fragment() {
 //            job.join()
 //            Log.d("zcc", "join")
 //        }
+    }
+
+    private suspend fun openCamera(
+        manager: CameraManager,
+        cameraId: String,
+        handler: Handler? = null
+    ) : CameraDevice = suspendCancellableCoroutine { cont ->
+//        manager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+//            override fun onOpened(p0: CameraDevice) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onDisconnected(p0: CameraDevice) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onError(p0: CameraDevice, p1: Int) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        }, handler)
     }
 
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,6 +10,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var activityMainBinding: ActivityMainBinding
 
+    companion object {
+        /** Combination of all flags required to put activity into immersive mode */
+        const val FLAGS_FULLSCREEN =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        /** Milliseconds used for UI animations */
+        const val ANIMATION_FAST_MILLIS = 50L
+        const val ANIMATION_SLOW_MILLIS = 100L
+        private const val IMMERSIVE_FLAG_TIMEOUT = 500L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,19 +38,7 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.fragmentContainer.postDelayed({
             activityMainBinding.fragmentContainer.systemUiVisibility = FLAGS_FULLSCREEN
         }, IMMERSIVE_FLAG_TIMEOUT)
-    }
 
-    companion object {
-        /** Combination of all flags required to put activity into immersive mode */
-        const val FLAGS_FULLSCREEN =
-            View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-
-        /** Milliseconds used for UI animations */
-        const val ANIMATION_FAST_MILLIS = 50L
-        const val ANIMATION_SLOW_MILLIS = 100L
-        private const val IMMERSIVE_FLAG_TIMEOUT = 500L
+        DelegateExample().testFunction()
     }
 }
