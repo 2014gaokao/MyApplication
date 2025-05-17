892d86622a5983244d2daaf0df071bde14c8b625
2014gaokao <2225052023@qq.com>
2025-05-18 00:48:02 +0800

    no default precision defined for variable 'v_texCoord'

--- app/src/main/cpp/GrayRender.h
+++ app/src/main/cpp/GrayRender.h
@@ -16,6 +16,7 @@ private:
 "#version 300 es\n"             \
 "#extension GL_OES_EGL_image_external_essl3 : require\n" \
 "#extension GL_EXT_YUV_target : require\n"               \
+"precision mediump float;\n"                         \
 "in vec2 v_texCoord;\n"         \
 "uniform sampler2D sTexture;\n"                    \
 "out vec4 outColor;\n"          \
@@ -25,3 +26,8 @@ private:
 "   float weightMean = outColor.r * 0.3 + outColor.g * 0.59 + outColor.b * 0.11;\n" \
 "   outColor.r = outColor.g = outColor.b = weightMean;\n"                    \
 "}\n"
+
+
+//"precision mediump float;\n"
+//Could not compile fragment shader:
+//0:4: S0032: no default precision defined for variable 'v_texCoord'
